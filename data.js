eb3b4852f3e614b1e2160422b4da173c833a92bf
Smit <smitmehta077@gmail.com>
2023-05-23 17:03:14 +0530

    basic-api done

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    {name: 'Smit Mehta', email: 'smitmehta@gmail.com', contact: 94237627276},
+    {name: 'Smit Mehta', email: 'smitmehta@gmail.com', contact: 94237627276},
+    {name: 'Smit Mehta', email: 'smitmehta@gmail.com', contact: 94237627276}
+]
+module.exports = data;
--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log(`Smit Mehta`)
-console.log(100+20);
+const http = require("http");
+const data = require("./data")
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'application\json'});
+    res.write(JSON.stringify(data));
+    res.end();
+}).listen(5500)
