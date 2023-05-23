dddf3f54acf6d727d823ee5be29f2ae07abb7fd8
Smit <smitmehta077@gmail.com>
2023-05-23 16:48:58 +0530

    Done

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-const http = require("http");
-// the createServer can also take the whole function as a parameter using arrow function
-http.createServer((req, res) =>{
-    res.write(`<h1>Hello this is smit mehta here</h1>`);
-    res.end();
-}).listen(4500);
- 
+console.log(`Smit Mehta`)
+console.log(100+20);
